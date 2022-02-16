16dddc3c41f2650cf47d8c5d70b388a28d013d3a
IDRIS ABDELLAH <50142372+abdellah-idris@users.noreply.github.com>
2022-02-17 00:20:08 +0100

    KNN
    A  KNN example with a test on a huge dataset (14 millions row)

--- MYrdbms.py
+++ MYrdbms.py
@@ -2,7 +2,7 @@ import pandas as pd
 #r stands for "raw" and will cause backslashes in the string
 #to be interpreted as actual backslashes rather than special characters.
 df = pd.read_csv(r"C:\Users\idris\OneDrive\Bureau\Study\s6\yellow_tripdata_2009-01.csv")
+print(df)
 print(df.info())
-#print(df["vendor_name "].values)
 x = df["vendor_name"]
 print(len(x))
